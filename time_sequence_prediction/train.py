1e1f8795514aa16f8a2ce517c77b99eaf273b9b4
Nathan Pierce <norsegaud@gmail.com>
2026-02-05 13:20:35 +0000

    Added weights_only=False to the torch.load call since the data file contains numpy arrays, which PyTorch 2.6 blocks by default for security.

--- time_sequence_prediction/train.py
+++ time_sequence_prediction/train.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     np.random.seed(0)
     torch.manual_seed(0)
     # load data and make training set
-    data = torch.load('traindata.pt')
+    data = torch.load('traindata.pt', weights_only=False)
     input = torch.from_numpy(data[3:, :-1])
     target = torch.from_numpy(data[3:, 1:])
     test_input = torch.from_numpy(data[:3, :-1])
